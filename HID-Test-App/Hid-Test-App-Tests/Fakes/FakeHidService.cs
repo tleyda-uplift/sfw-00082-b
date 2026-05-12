24f072dde1123aab917985622db315e808a1b8b3
Troy Leyda <troy.leyda@p3uplift.com>
2026-05-12 13:54:55 -0500

    Initial unit test project commit

---
+++ HID-Test-App/Hid-Test-App-Tests/Fakes/FakeHidService.cs
@@ -0,0 +1,50 @@
+﻿using HID_Test_App.Services;
+using HidSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hid_Test_App_Tests.Fakes
+{
+    public class FakeHidService : IHidService
+    {
+        bool _connected = false;
+
+        public bool Connected => _connected;
+
+        public event EventHandler<bool> ConnectionChanged;
+
+        public bool Connect(int vendorId, int productId)
+        {
+            _connected = true;
+            ConnectionChanged?.Invoke(this, _connected);
+            return true;
+        }
+
+        public void Disconnect()
+        {
+            _connected = false;
+            ConnectionChanged?.Invoke(this, _connected);
+        }
+
+        public byte[] GetReport(byte reportId)
+        {
+            return [];
+        }
+
+        public byte[] GetStatusReport()
+        {
+            return [];
+        }
+
+        public void Write(byte reportId, byte[] data)
+        {
+            
+        }
+
+        public void WriteConfigurationReport(byte[] data)
+        {
+            
+        }
+    }
+}
---
+++ HID-Test-App/Hid-Test-App-Tests/Presenters/TestMainPresenter.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using HID_Test_App.Presenters;
+using HID_Test_App.Views;
+using Hid_Test_App_Tests.Fakes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hid_Test_App_Tests.Presenters
+{
+    internal class TestMainForm : IMainForm
+    {
+        string _vendorId;
+        string _productId;
+        bool _connectEnabled;
+        bool _disconnectEnabled;
+
+        public string VendorId { get => _vendorId; set => _vendorId = value; }
+        public string ProductId { get => _productId; set => _productId = value; }
+        public bool ConnectEnabled { get => _connectEnabled; set => _connectEnabled = value; }
+        public bool DisconnectEnabled { get => _disconnectEnabled; set => _disconnectEnabled = value; }
+
+        public event EventHandler ConnectClicked;
+        public event EventHandler DisconnectClicked;
+
+        public void ClickConnect()
+        {
+            ConnectClicked?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void ClickDisconnect()
+        {
+            DisconnectClicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class TestMainPresenter
+    {
+        [Fact]
+        public void MainPresenter_DefaultsVendorAndProductIds()
+        {
+            var form = new TestMainForm();
+            var presenter = new MainPresenter(form, new FakeHidService());
+
+            form.VendorId.Should().Be("2047");
+            form.ProductId.Should().Be("3737");
+        }
+    }
+}
